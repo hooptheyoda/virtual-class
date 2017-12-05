4a84620452fe393bd31e23b28de640d8ed4843ee
George Donald <dongsy326@gmail.com>
2017-12-05 13:33:09 -0500

    completed classroom schedule

--- app/controllers/classroom_schedules_controller.rb
+++ app/controllers/classroom_schedules_controller.rb
@@ -3,16 +3,39 @@ class ClassroomSchedulesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    binding.pry
     classroom = Classroom.find(params[:classroom_id])
-    classroom.classroom_schedules.create(cs_params)
-    redirect_to classroom_path
+    begin
+      prm = cs_params
+      cs = ClassroomSchedule.new(prm)
+
+      sf=mt(cs.from)
+      st=mt(cs.to)
+      if sf >= st
+        raise "from time must less than to time"
+      end
+
+      classroom.classroom_schedules.each do |s|
+        if (s.weekday == cs.weekday)
+          df=mt(s.from)
+          dt=mt(s.to)
+
+          if((sf<df && st>df) || (sf<dt && st>dt) || (df<sf && dt>sf) || (df<st && dt>st) || (sf==df && st==dt))
+            raise "There is a time conflict in the schedule"
+          end
+        end
+      end
+
+      classroom.classroom_schedules.create(prm)
+      redirect_to classroom_path(classroom)
+    rescue  Exception => e
+      redirect_to classroom_path(classroom), notice: e.message
+    end
   end
 
   def destroy
     classroom_schedule = ClassroomSchedule.find(params[:id])
     classroom_schedule.destroy
-    redirect_to classroom_path
+    redirect_to classroom_path(Classroom.find(classroom_schedule.classroom_id))
   end
 
   private
@@ -20,4 +43,7 @@ class ClassroomSchedulesController < ApplicationController
     params.require(:classroom_schedule).permit(:weekday, :from, :to)
   end
 
+  def mt(t)
+    t.hour*60+t.min
+  end
 end

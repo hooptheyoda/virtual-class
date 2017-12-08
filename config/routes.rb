97a706c4669c7259a64d4913d764384ce7f9f649
Raven <ravenshooper@gmail.com>
2017-12-08 15:58:08 -0500

    everything works together!

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   get 'welcome/index'
 	root to: 'welcome#index'
   post '/check_conflicts', to: 'welcome#check_conflicts'
-  get '/classrooms/:id/play', to: 'classrooms#play'
+  get '/classrooms/:id/play', to: 'chat_rooms#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

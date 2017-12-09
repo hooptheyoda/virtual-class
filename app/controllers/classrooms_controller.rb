9c2f542222a02b5a72a8d7e3ab773ea7493b89c7
George Donald <dongsy326@gmail.com>
2017-12-09 08:21:04 -0500

    added some code to create a chat room with the classroom's id  when a user enters a classroom but there is chat room with the classroom's id, and restyled the white board's and chat room's elements. Chat room works, but white board still doesn't work.

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,10 +1,12 @@
-require 'pry'
-
 class ClassroomsController < ApplicationController
   before_action :authenticate_user!
 
   def show
     @classroom = Classroom.find(params[:id])
+    if(@classroom==nil)
+      redirect_to root_path
+      return
+    end
   end
 
   def new
@@ -13,35 +15,49 @@ class ClassroomsController < ApplicationController
 
   def edit
     @classroom = Classroom.find(params[:id])
+    if(@classroom==nil)
+      redirect_to root_path
+      return
+    end
   end
 
   def create
     @classroom = Classroom.new(classroom_params)
-    if @classroom.save
-      redirect_to root_path
+    if(@classroom!=nil)
+      if @classroom.save
+        redirect_to root_path
+      else
+        redirect_to new_classroom_path, notice: err_msg(@classroom)
+      end
     else
-      redirect_to new_classroom_path, notice: err_msg(@classroom)
+      redirect_to root_path
     end
   end
 
   def update
     @classroom = Classroom.find(params[:id])
-    if(@classroom.update(classroom_params))
-      redirect_to root_path
+    if(@classroom!=nil)
+      if(@classroom.update(classroom_params))
+        redirect_to root_path
+      else
+        redirect_to edit_classroom_path, notice: err_msg(@classroom)
+      end
     else
-      redirect_to edit_classroom_path, notice: err_msg(@classroom)
+      redirect_to root_path
     end
   end
 
   def destroy
     @classroom = Classroom.find(params[:id])
-    @classroom.destroy
+    if(@classroom!=nil)
+      @classroom.destroy
+    end
     redirect_to root_path
   end
 
   def play
   end
-  
+
   private
   def classroom_params
     params.require(:classroom).permit(:subject, :teacher_id, user_ids:[])

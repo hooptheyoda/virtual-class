78ffa6f6adece58fb0b0e39f0785c24601f51bce
Raven <ravenshooper@gmail.com>
2018-01-16 17:07:47 -0500

    precompile again

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,27 @@
+# These assets are from Gems which aren't included in every page.
+    # So they must be included here
+    # instead of in the application.js and css manifests.
+  #   config.assets.precompile += %w(a-gem.css a-gem.js b-gem.js)
+  #
+  #   # This block includes all js/css files, including gems,
+  # # under: app/assets, vendor/assets, lib/assets
+  # # and excluding partial files (e.g. "_file.sass")
+  # config.assets.precompile << Proc.new { |path|
+  #   if path =~ /\.(css|js)\z/
+  #     full_path = Rails.application.assets.resolve(path).to_path
+  #     asset_paths = %w( app/assets vendor/assets lib/assets)
+  #     if (asset_paths.any? {|ap| full_path.include? ap}) &&
+  #         !path.starts_with?('_')
+  #       puts "\tIncluding: " + full_path
+  #       true
+  #     else
+  #       puts "\tExcluding: " + full_path
+  #       false
+  #     end
+  #   else
+  #     false
+  #   end
+  # }
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
